113718d7f3463cc0e42bb534b5de3fd17891bfaa
Ayomide Jones <ayormeday@gmail.com>
2019-11-07 13:59:31 +0100

    stretch task completed on queries.sql

--- queries.sql
+++ queries.sql
@@ -30,3 +30,24 @@ JOIN Customer as c
 ON c.Id = o.CustomerId 
 JOIN Employee as e 
 ON e.id = o.EmployeeId;
+
+
+
+## Stretch Problems
+
+- In [SQL Try Editor at W3Schools.com](https://www.w3schools.com/Sql/tryit.asp?filename=trysql_select_top):
+- Displays CategoryName and a new column called Count that shows how many products are in each category. Shows 9 records.
+
+SELECT CategoryName, COUNT(p.ProductName) as ProductsCount 
+FROM Products as p 
+JOIN Categories as c 
+ON p.CategoryID = c.CategoryID 
+GROUP BY CategoryName;
+
+- Display OrderID and a column called ItemCount that shows the total number of products placed on the order. Shows 196 records.
+
+SELECT OrderID, COUNT(p.ProductName) as ItemCount 
+FROM OrderDetails as od 
+JOIN Products as p 
+ON od.ProductID = p.ProductID 
+GROUP BY OrderID;
